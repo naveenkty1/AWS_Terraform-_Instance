3f764ae7f2cc6fa8177f086ca52aa807095dfce6
root <root@ip-172-31-22-175.us-east-2.compute.internal>
2019-03-29 19:08:12 +0000

    filles added

---
+++ main.tf
@@ -0,0 +1,42 @@
+resource "aws_instance" "example" {
+  # Ubuntu Server 18.04 LTS (HVM), SSD Volume Type in us-east-1
+  ami = "ami-0e7589a8422e3270f"
+  instance_type = "t2.micro"
+  key_name = "aws_key"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+
+  tags {
+    Name = "terraform-example"
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE THE SECURITY GROUP THAT'S APPLIED TO THE EC2 INSTANCE
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  # Inbound HTTP from anywhere
+  ingress {
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Inbound SSH from anywhere
+  ingress {
+    from_port = "${var.ssh_port}"
+    to_port = "${var.ssh_port}"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
